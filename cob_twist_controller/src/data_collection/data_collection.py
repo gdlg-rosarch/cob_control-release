83bccc80ca44b06b21ab8b2f2eac2e2fe7d15011
ipa-fxm <fxm@ipa.fhg.de>
2016-04-01 17:37:23 +0200

    Rebase from 'upstream'

--- cob_twist_controller/src/data_collection/data_collection.py
+++ cob_twist_controller/src/data_collection/data_collection.py
@@ -39,7 +39,7 @@ from std_msgs.msg import Float64MultiArray
 from geometry_msgs.msg import TwistStamped
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from cob_obstacle_distance.msg import ObstacleDistances
+from cob_control_msgs.msg import ObstacleDistances
 import tf
 
 DEL = ','
@@ -139,7 +139,7 @@ class ObstacleDistanceDataKraken(DataKraken):
             header = []
             header.append('time stamp')
             header.append('time delta')
-            header.append('frame_of_interest')
+            header.append('link_of_interest')
             header.append('obstacle')
             header.append('distance')
             header.append('frame_vector_x')
@@ -158,7 +158,7 @@ class ObstacleDistanceDataKraken(DataKraken):
         delta = now - self.start_
         for elem in data.distances:
             data_row = [now.strftime(DATE_TIME_FMT), '%.3f' % delta.total_seconds()]
-            data_row.append(elem.frame_of_interest)
+            data_row.append(elem.link_of_interest)
             data_row.append(elem.obstacle_id)
             data_row.append(elem.distance)
             data_row.append(elem.frame_vector.x)
